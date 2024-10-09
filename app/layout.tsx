5c391bb98a8a95ae8b36e5922fee3356ba82c145
César O. Araújo <cesaro.araujo@Cesars-MacBook-Air.local>
2024-10-08 22:05:04 -0300

    fix lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/components/lib/utils/supabase/check-env-vars";
+
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import { hasEnvVars } from "@/lib/utils/supabase/check-env-vars";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/components/lib/utils/supabase/check-env-vars";
+import { hasEnvVars } from "@/lib/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
